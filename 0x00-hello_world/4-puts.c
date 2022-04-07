0c472d8d039f5bb534f4e03590e7a2c46312d1d0
Rediet Pawlos <rpaulos8edi@gmail.com>
2022-04-07 14:26:17 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-{/**
+/**
 *main - entry point
 *
 *Return: 0 after printing the function
 */
-outs("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
