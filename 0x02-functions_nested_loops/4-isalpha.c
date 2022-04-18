fcc1f2121880ccd160802b160989354cedfb671b
REDXIXR <101404001+REDXIXR@users.noreply.github.com>
2022-04-18 17:45:33 +0300

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,24 @@
 #include "main.h"
 
 /**
-*
-*
-*/
-int _islower(int c)
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ *
+ *@c: The int to print
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+
 {
-if (c >= 'a' && c <=)
-return(1);
+
+if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+{
+return (1);
+}
+
 else
-return(0);
+{
+return (0);
+}
+
 }
